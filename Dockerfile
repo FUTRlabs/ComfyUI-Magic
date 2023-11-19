5b901748f576a73e8ea95f14410fcaf3b3e8d397
Keith Hanson <keith@keithhanson.io>
2023-11-19 10:04:11 -0600

    First public commit.

---
+++ Dockerfile
@@ -0,0 +1,70 @@
+FROM pytorch/pytorch:2.1.0-cuda11.8-cudnn8-runtime
+
+RUN apt update -y
+RUN DEBIAN_FRONTEND=noninteractive TZ=America/Chicago apt install -y sudo build-essential iproute2 wget ncurses-bin figlet toilet vim nano tig curl git htop zsh ffmpeg tmux jq ca-certificates gnupg
+
+RUN mkdir -p /etc/apt/keyrings
+
+RUN curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+RUN echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+RUN apt update 
+RUN apt install -y nodejs 
+
+RUN cd /root && npm install request chokidar form-data ws glob express axios @aws-sdk/client-s3 @aws-sdk/credential-providers dotenv bull
+
+RUN pip install gradio opencv-python kornia loguru scikit-image onnx onnxruntime-gpu lpips ultralytics python_bidi arabic_reshaper 
+RUN pip install torchvision gitpython timm addict yapf insightface numba
+
+RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+COPY ./.zshrc /root/.zshrc
+
+WORKDIR  /workspace/
+#COPY ./src/HiFiFace-pytorch /workspace/src/HiFiFace-pytorch
+
+RUN git clone https://github.com/comfyanonymous/ComfyUI.git /workspace/ComfyUI
+RUN git clone https://github.com/ltdrdata/ComfyUI-Manager.git /workspace/ComfyUI/custom_nodes/ComfyUI-Manager
+
+
+WORKDIR /workspace
+RUN cd /workspace/ComfyUI && pip install -r requirements.txt
+RUN cd /workspace/ComfyUI/custom_nodes/ComfyUI-Manager && pip install -r requirements.txt
+
+WORKDIR /workspace/ComfyUI/custom_nodes
+RUN git clone https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git && cd ComfyUI-Inspire-Pack && git reset --hard c380f3c7d554940f9a01f701d2b8f512b0346021 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack.git && cd ComfyUI-Impact-Pack && git reset --hard b6dcea19d980f7ca36c2c6bee1b51e024a233d6b && ( pip install -r requirements.txt || true ) 
+RUN git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus.git && cd ComfyUI_IPAdapter_plus && git reset --hard ee85b4cff6b274d5b2b58cbfcfdd57d5790d68de && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/storyicon/comfyui_segment_anything.git && cd comfyui_segment_anything && git reset --hard f2283d4e4207d4352dfa2fe00ee952dc4918e6ef && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/Gourieff/comfyui-reactor-node.git && cd comfyui-reactor-node && git reset --hard 9404b14b8c1e35845967c9bd4455fcccd28705e6 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/WASasquatch/was-node-suite-comfyui.git && cd was-node-suite-comfyui && git reset --hard 4e53775e650a7e2d2d1d73056bb914d7edc57f69 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git && cd ComfyUI-Custom-Scripts && git reset --hard 27555d4f71bb4e24b87571f89eab2b4a06677bb6 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/RockOfFire/ComfyUI_Comfyroll_CustomNodes.git && cd ComfyUI_Comfyroll_CustomNodes && git reset --hard 9c9f7aaa06eaa903fa18feac31735fc7591f7a08 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet && cd ComfyUI-Advanced-ControlNet && git reset --hard b5e77ecc3f8cd274f13996bf05816c601d90006f && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite && cd ComfyUI-VideoHelperSuite && git reset --hard aba622c97f1a656164658f0d75a801f1ac35a4fe && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/Fannovel16/comfyui_controlnet_aux && cd comfyui_controlnet_aux && git reset --hard 58ce1e01ebb13a38f6f3ab32a31bd3beb065e0d8 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/jags111/efficiency-nodes-comfyui && cd efficiency-nodes-comfyui && git reset --hard 8cae155233444c9f16172389dedbe38d386f1b15 && ( pip install -r requirements.txt || true )
+RUN git clone https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git && cd ComfyUI-AnimateDiff-Evolved && git reset --hard 203313e1ea52c2c76d38dbaaf5790d56eb601ebb && ( pip install -r requirements.txt || true )
+
+RUN mkdir -p /workspace/ComfyUI/models/facerestore_models/ && wget -nc -O /workspace/ComfyUI/models/facerestore_models/codeformer.pth https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth
+#RUN wget -nc -O /workspace/ComfyUI/models/facerestore_models/GFPGANv1.4.pth  https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth
+
+# Cached prior to deletion
+RUN mkdir -p /workspace/ComfyUI/models/insightface/ 
+COPY ./models/inswapper_128.onnx /workspace/ComfyUI/models/insightface/inswapper_128.onnx
+
+# Add Tini
+ENV TINI_VERSION v0.19.0
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /tini
+RUN chmod +x /tini
+ENTRYPOINT ["/tini", "--"]
+
+RUN wget https://github.com/mikefarah/yq/releases/download/v4.35.2/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+
+COPY ./extra_model_paths.yml /extra_model_paths.yml
+COPY ./extra_downloads.yml /extra_downloads.yml
+
+COPY ./magic /bin/magic
+COPY ./.env /root/.env
+COPY ./heartbeat.js /root/heartbeat.js
+CMD ["/bin/magic"]
---
+++ heartbeat.js
@@ -0,0 +1,306 @@
+const { exec } = require('child_process');
+const { spawn } = require('child_process');
+
+const fs = require('fs');
+
+const axios = require('axios');
+const FormData = require('form-data');
+
+const Queue = require('bull');
+
+const path = require('path');
+
+require('dotenv').config();
+
+const Redis = require("ioredis");
+
+const production = true;
+
+
+const {
+  S3Client,
+  PutObjectCommand,
+  CreateBucketCommand,
+  DeleteObjectCommand,
+  DeleteBucketCommand,
+  paginateListObjectsV2,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
+
+
+const { fromEnv } = require("@aws-sdk/credential-providers");
+
+const s3Client = new S3Client({region: "us-east-2", credentials: fromEnv()});
+
+// Helper function to upload content to S3
+async function uploadToS3(bucketName, key, buffer) {
+  return s3Client.send(
+    new PutObjectCommand({
+      Bucket: bucketName,
+      Key: key,
+      Body: buffer,
+    })
+  );
+}
+
+
+const supplierID = process.env.SUPPLIER_ID;
+
+if(supplierID == null) {
+  console.log("YOU MUST PROVIDE A SUPPLIER_ID TO PARTICIPATE IN MINETHEFUTR.COM.");
+}
+
+// Get the default IP of the host for developers to run the queue and this locally
+const getDefaultRoute = async () => {
+  return new Promise((resolve, reject) => {
+    if(!production) {
+      exec("/sbin/ip route|awk '/default/ { print $3 }'", (error, stdout, stderr) => {
+        if (error) {
+          return reject(error);
+        }
+        if (stderr) {
+          return reject(new Error(stderr));
+        }
+        resolve(stdout.trim() + ":3000");
+      });
+    } else {
+      resolve("queue.minethefutr.com:3000");
+    }
+  });
+};
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function waitForComfy() {
+    const url = 'http://0.0.0.0:8188';
+
+    while (true) {
+        try {
+            const response = await axios.get(url);
+            if (response.status === 200) {
+                console.log('Received 200 OK - COMFYUI READY');
+                break;
+            }
+        } catch (error) {
+            console.error('Error or non-200 response, retrying...');
+        }
+
+        // Wait for a short period before retrying (e.g., 5 seconds)
+        await sleep(5000);
+    }
+}
+
+async function mainLoop() {
+  await waitForComfy();
+  const defaultRoute = await getDefaultRoute();
+
+  const response = await axios.get(`http://${defaultRoute}/register/${supplierID}`);
+
+  if (response.data && response.data.ok === false) {
+    throw new Error("The supplier registration failed. Please verify the SUPPLIER_ID has been provided via docker environment variables.");
+  } else {
+    console.log("Registration succeeded.");
+  }
+
+  const connectionString = `redis://${supplierID}:@${defaultRoute.split(":")[0]}:6379`;
+  
+  var manualRedis = new Redis(connectionString, {
+    reconnectOnError: function(err) {
+      console.log("FUTR: ERROR CONNECTING TO QUEUE");
+      console.log(err.message);
+
+      return axios.get(`http://${defaultRoute}/register/${supplierID}`).then( () => {
+       return true; 
+      });
+    },
+    //enableReadyCheck: false
+  });
+
+  try { 
+    console.log("Connecting to Queue...");
+
+    const supplierQueue = new Queue("supplierQueue", connectionString);
+
+    await supplierQueue.process(async function (job, done) {
+      console.log(`Caught a job: ${job.id}`);
+      const workflow = job.data.workflow;
+
+      Object.keys(workflow).forEach((key) => {
+        const obj = workflow[key];
+        if (obj.class_type === "SaveImage") {
+          obj.inputs.filename_prefix = "futr_" + job.id + "_";
+        }
+      });
+
+      const consumer_id = job.data.owner_key;
+
+      const sendPrompt = await axios.post("http://localhost:8188/prompt", JSON.stringify({"prompt": workflow}), {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).catch(
+        function (error) {
+          console.log("COMFY ERROR PROCESSING WORKFLOW:",error)
+          done(new Error("COMFY WORKFLOW ERROR:\n\n" + getLast100LogLines()));
+          return null;
+        }
+      ) 
+
+      if(sendPrompt) {
+        const outputPath = '/storage/ComfyUI/output'; // replace with your folder path
+
+        //TODO: Watch for more than one file?
+        const filePath =  `${outputPath}/futr_${job.id}__00001_.png`;
+
+        var lastFileSizeInBytes = 0;
+        var sameReadingCount = 0;
+
+        var gpuReadings = [];
+
+        while(true) {
+          // Check if the file exists
+          if (fs.existsSync(filePath)) {
+
+            // Get the stats of the file
+            const stats = fs.statSync(filePath);
+            const fileSizeInBytes = stats.size;
+
+            // Record the new last filesize
+            if(fileSizeInBytes > 0) {
+
+              if(lastFileSizeInBytes != fileSizeInBytes) {
+                lastFileSizeInBytes = fileSizeInBytes;
+                //reset the reading count
+                sameReadingCount = 0;
+              } else {
+                sameReadingCount += 1;
+              }
+
+            }
+
+            //Break out of the loop if we've found our file.
+            if(sameReadingCount > 5) {
+              break;
+            }
+
+          } else {
+            //console.log('File does not exist.');
+          }
+
+          try {
+            var gpuResults = await getGpuInfo();
+            gpuReadings.push(gpuResults);
+          } catch (error) {
+            console.log("Cannot retrieve GPU Readings.");
+            console.log(error);
+          }
+
+          await sleep(50);
+        } //while
+
+        try {
+          // Read the file content
+          const fileStream = fs.createReadStream(filePath);
+
+          const filename = path.basename(filePath);
+
+          const fileKey = `output/${job.id}_${filename}`;
+
+          const result = await uploadToS3("futr-workflows", fileKey, fileStream);
+
+          const finalUrl = `https://futr-workflows.s3.us-east-2.amazonaws.com/${fileKey}`;
+
+          //TODO: Support multiple images!
+
+          console.log(`Job completed and uploaded: ${finalUrl}`);
+
+          done(null, {images: [finalUrl,], supplier_id: supplierID, gpu_stats: gpuReadings});
+        } catch (readError) {
+          console.error("Error:", readError);
+        }
+      }
+
+    }); //queue.process
+  } catch(connectError) {
+    console.log(`COULD NOT CONNECT TO QUEUE... Trying again in 5 seconds...`);
+  } 
+};
+
+
+function getGpuInfo() {
+  return new Promise((resolve, reject) => {
+    exec('nvidia-smi --query-gpu=timestamp,driver_version,name,memory.total,memory.used,memory.free,pstate,temperature.gpu,power.draw,power.limit --format=csv,noheader', (error, stdout, stderr) => {
+      if (error) {
+        console.log('Error fetching GPU info:');
+        console.log(stderr);
+        reject(new Error('Failed to fetch GPU info'));
+      } else {
+        resolve(stdout.trim());
+      }
+    });
+  });
+}
+
+// Function to fetch Python information
+function getPythonInfo(callback) {
+  exec('python --version', (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error fetching Python version:', stderr);
+      callback(null);
+    } else {
+      const version = stdout || stderr; // python --version writes to stderr
+      exec('pip list --format=json', (pipError, pipStdout, pipStderr) => {
+        if (pipError) {
+          console.error('Error fetching pip list:', pipStderr);
+          callback(version.trim(), null);
+        } else {
+          const libraries = JSON.parse(pipStdout);
+          callback(version.trim(), libraries);
+        }
+      });
+    }
+  });
+}
+
+let comfyUiProcess;
+let logs = [];
+
+function startComfyUi() {
+  comfyUiProcess = spawn('python', ['/storage/ComfyUI/main.py', '--listen', '0.0.0.0', '--extra-model-paths-config', '/extra_model_paths.yml']);
+
+  comfyUiProcess.stdout.on('data', (data) => {
+    processLog(data);
+  });
+
+  comfyUiProcess.stderr.on('data', (data) => {
+    processLog(data);
+  });
+
+  comfyUiProcess.on('close', (code) => {
+    console.log(`ComfyUI process exited with code ${code}`);
+    startComfyUi(); // Restart ComfyUI
+  });
+}
+
+function processLog(data) {
+  const logLine = data.toString();
+  console.log(logLine); // Print the log line
+  logs.push(logLine);
+  if (logs.length > 100) {
+    logs.shift(); // Remove the oldest log line if more than 100 lines are stored
+  }
+}
+
+function getLast100LogLines() {
+  return logs.join('\n');
+}
+
+startComfyUi();
+
+(async function main() {
+  while(true) {
+    await mainLoop();
+  }
+})();
